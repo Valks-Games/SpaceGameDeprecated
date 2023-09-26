4c11db95fe21e550912131c709d6838721e5c635
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-09-26 18:05:17 -0500

    Player movement + engine particles

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -2,8 +2,53 @@ namespace SpaceGame;
 
 public partial class Player : RigidBody2D
 {
+    [Export] float thrustAcceleration = 2.0f;
+    [Export] float rotateAcceleration = 0.1f;
+
+    List<GpuParticles2D> engineParticles = new();
+
+    public override void _Ready()
+    {
+        foreach (GpuParticles2D particles in GetNode("Engine Particles").GetChildren())
+        {
+            particles.Emitting = false;
+            engineParticles.Add(particles);
+        }
+    }
+
     public override void _PhysicsProcess(double delta)
     {
-        AddConstantCentralForce(Vector2.Up);
+        if (Input.IsActionPressed("move_left"))
+        {
+            AngularVelocity -= rotateAcceleration;
+        }
+
+        if (Input.IsActionPressed("move_right"))
+        {
+            AngularVelocity += rotateAcceleration;
+        }
+
+        if (Input.IsActionPressed("move_up"))
+        {
+            ApplyCentralImpulse(Vector2.Up.Rotated(Rotation) * thrustAcceleration);
+        }
+
+        if (Input.IsActionJustPressed("move_up"))
+        {
+            engineParticles.ForEach(x =>
+            {
+                x.Lifetime = 1f;
+                x.Emitting = true;
+            });
+        }
+
+        if (Input.IsActionJustReleased("move_up"))
+        {
+            engineParticles.ForEach(x =>
+            {
+                x.Lifetime = 0.5f;
+                x.Emitting = false;
+            });
+        }
     }
 }
