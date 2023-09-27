2ff662154310dde77036f04e6886af1181716834
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-09-27 15:25:21 -0500

    Improve performance of EnemyShip.cs

--- Scripts/Enemies/EnemyShip.cs
+++ Scripts/Enemies/EnemyShip.cs
@@ -5,28 +5,24 @@ public partial class EnemyShip : RigidBody2D
     [Export] float thrustAcceleration = 2.0f;
     [Export] float rotateAcceleration = 0.1f;
 
+    RigidBody2D player;
     List<GpuParticles2D> engineParticles = new();
 
     public override void _Ready()
 	{
+        player = GetTree().GetFirstNodeInGroup("Player") as RigidBody2D;
+
         foreach (GpuParticles2D particles in GetNode("Engine Particles").GetChildren())
         {
-            particles.Emitting = false;
+            particles.Emitting = true;
             engineParticles.Add(particles);
         }
     }
 
     public override void _PhysicsProcess(double delta)
     {
-        RigidBody2D player = GetTree().GetFirstNodeInGroup("Player") as RigidBody2D;
-
         //Position += (player.Position - Position) / 50;
         //ApplyCentralImpulse(Vector2.Up * thrustAcceleration);
-        engineParticles.ForEach(x =>
-        {
-            x.Lifetime = 1f;
-            x.Emitting = true;
-        });
 
         LookAt(player.Position);
         ApplyCentralImpulse(Vector2.Right.Rotated(Rotation) * thrustAcceleration);
