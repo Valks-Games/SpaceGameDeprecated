b4c41cf0b1f710b233fd9b64b352931562daf851
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-09-30 20:29:59 -0500

    Organize particles into folder

--- Scripts/Static/Prefabs.cs
+++ Scripts/Static/Prefabs.cs
@@ -1,10 +1,17 @@
 namespace SpaceGame;
 
+public static class Particles
+{
+    public static PackedScene ProjectileHit { get; } = Load("Particles/projectile_hit");
+    public static PackedScene Explosion { get; } = Load("Particles/explosion");
+
+    static PackedScene Load(string path) =>
+        GD.Load<PackedScene>($"res://Scenes/Prefabs/{path}.tscn");
+}
+
 public static class Prefabs
 {
     public static PackedScene Shield { get; } = Load("Components/shield");
-    public static PackedScene ProjectileHit { get; } = Load("projectile_hit");
-    public static PackedScene Explosion { get; } = Load("explosion");
     public static PackedScene Options { get; } = Load("UI/options");
 
     static PackedScene Load(string path) =>
